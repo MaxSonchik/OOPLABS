e833620ed1166cb9de19e1a4876dea6fbe1ede1f
MaxSonchik <97745950+MaxSonchik@users.noreply.github.com>
2024-10-14 11:34:56 +0300

    develop

--- Laba2/Fines_i_Ferb_corp.cpp
+++ Laba2/Fines_i_Ferb_corp.cpp
@@ -3,13 +3,12 @@
 #include <limits>
 using namespace std;
 
-
 class Publication {
 protected:
     string title; 
-    float price;  
+    float price;   
 public:
-    
+
     void getTitleAndPrice() {
         while (true) {
             cout << "Enter price: ";
@@ -28,29 +27,47 @@ public:
         getline(cin, title); 
     }
 
-    
-    void putTitleAndPrice() const {
+    void putTitle() const {
         cout << "Title: " << title << endl;
-        cout << "Price: $" << price << endl;
+    }
+
+    float getPrice() const {
+        return price;
     }
 };
 
 
 class Sales {
 private:
-    float sales[3]; 
+    int booksSold[3];       
+    int audiobooksSold[3];  
 public:
-    
+ 
     void getdata() {
-        cout << "Enter sales for the last 3 months:" << endl;
+        cout << "Enter number of books sold for the last 3 months:" << endl;
         for (int i = 0; i < 3; ++i) {
             while (true) {
-                cout << "Month " << i + 1 << ": ";
-                cin >> sales[i];
+                cout << "Month " << i + 1 << " (books): ";
+                cin >> booksSold[i];
                 if (cin.fail()) {
                     cin.clear();
                     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                    cout << "Invalid input. Please enter a numeric value for sales." << endl;
+                    cout << "Invalid input. Please enter a numeric value." << endl;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        cout << "Enter number of audiobooks sold for the last 3 months:" << endl;
+        for (int i = 0; i < 3; ++i) {
+            while (true) {
+                cout << "Month " << i + 1 << " (audiobooks): ";
+                cin >> audiobooksSold[i];
+                if (cin.fail()) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << "Invalid input. Please enter a numeric value." << endl;
                 } else {
                     break;
                 }
@@ -58,108 +75,55 @@ public:
         }
     }
 
-    
-    void putdata() const {
-        cout << "Sales for the last 3 months:" << endl;
+
+    void calculateMonthlySales(float price) const {
         for (int i = 0; i < 3; ++i) {
-            cout << "Month " << i + 1 << ": $" << sales[i] << endl;
+            int totalBooksSold = booksSold[i];
+            int totalAudiobooksSold = audiobooksSold[i];
+
+            float monthlySales = (totalBooksSold + totalAudiobooksSold) * price;
+
+            cout << "Month " << i + 1 << ": $" << monthlySales << endl;
         }
     }
 };
 
 
-class Book : public Sales {
+class BookSales : public Sales {
 private:
-    int pageCount; 
-    Publication& pub;
-public:
-
-    Book(Publication& p) : pub(p) {}
-
-
-    void getdata() {
-
-        while (true) {
-            cout << "Enter page count: ";
-            cin >> pageCount;
-            if (cin.fail()) {
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                cout << "Invalid input. Please enter a numeric value for page count." << endl;
-            } else {
-                break;
-            }
-        }
-
-        Sales::getdata(); 
-    }
-
-
-    void putdata() const {
-        cout << "Book details:" << endl;
-        pub.putTitleAndPrice(); 
-        cout << "Page count: " << pageCount << " pages" << endl;
-        Sales::putdata(); 
-    }
-};
-
-
-class Type : public Sales {
-private:
-    float playTime; 
     Publication& pub; 
 public:
-    
-    Type(Publication& p) : pub(p) {}
+
+    BookSales(Publication& p) : pub(p) {}
+
 
     void getdata() {
-        while (true) {
-            cout << "Enter play time (in minutes): ";
-            cin >> playTime;
-            if (cin.fail()) {
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                cout << "Invalid input. Please enter a numeric value for play time." << endl;
-            } else {
-                break;
-            }
-        }
-
         Sales::getdata(); 
     }
 
-    
+
     void putdata() const {
-        cout << "Audiobook details:" << endl;
-        pub.putTitleAndPrice(); 
-        cout << "Play time: " << playTime << " minutes" << endl;
-        Sales::putdata(); 
+        pub.putTitle();
+        Sales::calculateMonthlySales(pub.getPrice()); 
     }
 };
 
 int main() {
     Publication pub; 
 
+
     cout << "Enter common data for publication (title and price):" << endl;
     pub.getTitleAndPrice();
 
-    Book myBook(pub);
-    Type myType(pub);
+
+    BookSales myBookSales(pub);
 
 
-    cout << "\nEnter specific data for book:" << endl;
-    myBook.getdata();
+    cout << "\nEnter sales data (books and audiobooks):" << endl;
+    myBookSales.getdata();
 
-    cout << "\nEnter specific data for type (audiobook):" << endl;
-    myType.getdata();
-
-
-    cout << "\nData for book:" << endl;
-    myBook.putdata();
-
- 
-    cout << "\nData for audiobook:" << endl;
-    myType.putdata();
+    cout << "\nSales data summary:" << endl;
+    myBookSales.putdata();
 
     return 0;
 }
